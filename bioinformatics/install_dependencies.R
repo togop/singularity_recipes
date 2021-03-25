0801cd90c5c9bd20609d6374a71136df24ed6814
Todor Gitchev <todor.gitchev@gmail.com>
2021-03-25 22:56:16 +0100

    update from frozen; remove redundant files

--- bioinformatics/install_dependencies.R
+++ bioinformatics/install_dependencies.R
@@ -10,26 +10,23 @@ install_dependencies <- function(packages){
     } else {
       print(paste("Package", package, " already installed."))
     }
-
   }
 }
 
 install_Bioc_dependencies <- function(packages){
   if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", quietly = TRUE)
-
   for (package in packages){
     if (!requireNamespace(package, quietly = TRUE)) {
       BiocManager::install(package, quietly = TRUE)
     } else {
       print(paste("Bioconductor package", package, " already installed."))
     }
-
   }
 }
 
-install_dependencies(packages = c("dplyr", "Seurat", "reshape", "data.table", "readr", "hash", "ggplot2",
-                                  "scales", "RColorBrewer", "gridExtra", "grid", "gtable", "plotly"))
+install_dependencies(packages = c("dplyr", "Seurat", "reshape", "data.table", "readr", "hash", "ggplot2","scales",
+                                  "RColorBrewer", "gridExtra", "grid", "gtable", "plotly"))
 
-install_Bioc_dependencies(packages = c("SingleR", "Gviz", "Organism.dplyr", "GenomicRanges", "GenomicFeatures", "affy"))
-# "backports",
+install_Bioc_dependencies(packages = c("SingleR", "celldex", "Gviz", "Organism.dplyr", "GenomicRanges",
+                                       "GenomicFeatures", "affy"))
